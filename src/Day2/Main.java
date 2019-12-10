3e2aaf561cf205b2d5593dfbb14f6366b1198b60
Sven Bergmann <sven.bergmann@rwth-aachen.de>
2019-12-10 08:16:56 +0100

    Finished Day 2.1

--- src/Day2/Main.java
+++ src/Day2/Main.java
@@ -11,27 +11,29 @@ public class Main {
         ArrayList<Integer> intCode = new ArrayList<>();
         String fileInput = ReadAndSaveUtils.returnFileContent("src/Day2/IntcodeInput");
         String[] strings = fileInput.split(",");
-        for(String string: strings) {
+        for (String string : strings) {
             intCode.add(Integer.parseInt(string));
         }
 
-        System.out.println(intCode);
+        intCode.set(1, 12);
+        intCode.set(2, 2);
+        int[] intCodeArray = new int[intCode.size()];
+        for (int i = 0; i < intCode.size(); i++) {
+            intCodeArray[i] = intCode.get(i);
+        }
 
-        for (int i = 0; i < intCode.size(); ) {
-            if (intCode.get(i) == 1) {
-                intCode.set(intCode.get(i+3), intCode.get(i + 1) + intCode.get(i + 2));
-                i += 4;
-            } else if (intCode.get(i) == 2) {
-                intCode.set(intCode.get(i + 3), intCode.get(i + 1) * intCode.get(i + 2));
-                i += 4;
-            } else if (intCode.get(i) == 99) {
-                i++;
+        for (int i = 0; i < intCodeArray.length; i += 4) {
+            if (intCodeArray[i] == 1) {
+                intCodeArray[intCodeArray[i + 3]] = intCodeArray[intCodeArray[i + 1]] + intCodeArray[intCodeArray[i + 2]];
+            } else if (intCodeArray[i] == 2) {
+                intCodeArray[intCodeArray[i + 3]] = intCodeArray[intCodeArray[i + 1]] * intCodeArray[intCodeArray[i + 2]];
+            } else if (intCodeArray[i] == 99) {
+                System.out.println(i);
+                System.out.println(intCodeArray[0]);
+                break;
             } else {
                 throw new IllegalArgumentException("Zahl passt nicht in das Pattern!");
             }
         }
-
-        System.out.println(intCode);
     }
-
 }
