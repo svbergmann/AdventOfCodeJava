c75c7e07fe382f8a7a0adde547d725d9339b6f2c
Sven Bergmann <sven.bergmann@rwth-aachen.de>
2019-12-10 08:30:04 +0100

    Finished Day 2.2

--- src/Day2/Main.java
+++ src/Day2/Main.java
@@ -35,5 +35,43 @@ public class Main {
                 throw new IllegalArgumentException("Zahl passt nicht in das Pattern!");
             }
         }
+
+        //Day2
+
+        int result = 0;
+        int nounResult=0;
+        int verbResult=0;
+        for(int noun=0; noun <100; noun++) {
+            for(int verb=0; verb<100; verb++) {
+                intCodeArray[1] = noun;
+                intCodeArray[2] = verb;
+                for (int i = 0; i < intCodeArray.length; i += 4) {
+                    if (intCodeArray[i] == 1) {
+                        intCodeArray[intCodeArray[i + 3]] = intCodeArray[intCodeArray[i + 1]] + intCodeArray[intCodeArray[i + 2]];
+                    } else if (intCodeArray[i] == 2) {
+                        intCodeArray[intCodeArray[i + 3]] = intCodeArray[intCodeArray[i + 1]] * intCodeArray[intCodeArray[i + 2]];
+                    } else if (intCodeArray[i] == 99) {
+                        result = intCodeArray[0];
+                        nounResult = noun;
+                        verbResult = verb;
+                        break;
+                    } else {
+                        break;
+                    }
+                }
+                if(result == 19690720) {
+                    break;
+                } else {
+                    for (int i = 0; i < intCode.size(); i++) {
+                        intCodeArray[i] = intCode.get(i);
+                    }
+                }
+            }
+            if(result == 19690720) {
+                break;
+            }
+        }
+
+        System.out.println(100 * nounResult + verbResult);
     }
 }
