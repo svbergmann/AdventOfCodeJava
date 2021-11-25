ec9da68e40ef93246beba03f11a01a1ad5454176
Sven Bergmann <sven.bergmann@rwth-aachen.de>
2021-11-25 14:38:14 +0100

    Prepared for 2021

---
+++ src/aoc2021/day1/Day1.java
@@ -0,0 +1,20 @@
+package aoc2021.day1;
+
+import utils.Day;
+
+public class Day1 extends Day {
+
+	public Day1() {
+		super(2021, 1);
+	}
+
+	@Override
+	public String resultPartOne() {
+		return null;
+	}
+
+	@Override
+	public String resultPartTwo() {
+		return null;
+	}
+}
---
+++ src/aoc2021/day2/Day2.java
@@ -0,0 +1,20 @@
+package aoc2021.day2;
+
+import utils.Day;
+
+public class Day2 extends Day {
+
+	public Day2() {
+		super(2021, 2);
+	}
+
+	@Override
+	public String resultPartOne() {
+		return null;
+	}
+
+	@Override
+	public String resultPartTwo() {
+		return null;
+	}
+}
