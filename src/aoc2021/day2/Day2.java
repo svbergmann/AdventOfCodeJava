37f0afa0187e385e9993acc25b44cb978aa92834
Sven Bergmann <sven.bergmann@rwth-aachen.de>
2021-12-02 06:55:55 +0100

    Finished Day2

--- src/aoc2021/day2/Day2.java
+++ src/aoc2021/day2/Day2.java
@@ -10,11 +10,33 @@ public class Day2 extends Day {
 
 	@Override
 	public String resultPartOne() {
-		return null;
+		var horizontal = 0;
+		var vertical = 0;
+		for (var s : this.input) {
+			var value = Integer.parseInt(s.substring(s.length() - 1));
+			if (s.startsWith("forward")) horizontal += value;
+			else if (s.startsWith("down")) vertical += value;
+			else if (s.startsWith("up")) vertical -= value;
+		}
+		return horizontal * vertical + "";
 	}
 
 	@Override
 	public String resultPartTwo() {
-		return null;
+		var horizontal = 0;
+		var depth = 0;
+		var aim = 0;
+		for (var s : this.input) {
+			var value = Integer.parseInt(s.substring(s.length() - 1));
+			if (s.startsWith("forward")) {
+				horizontal += value;
+				depth += aim * value;
+			} else if (s.startsWith("down")) {
+				aim += value;
+			} else if (s.startsWith("up")) {
+				aim -= value;
+			}
+		}
+		return horizontal * depth + "";
 	}
 }
