07b7d28138570bd20009ccb6efb3bf3edab5b969
Sven Bergmann <sven.bergmann@cae.de>
2021-12-06 12:53:50 +0100

    Prepared for Day7

---
+++ src/aoc2021/day7/Day7.java
@@ -0,0 +1,20 @@
+package aoc2021.day7;
+
+import utils.Day;
+
+public class Day7 extends Day {
+
+    public Day7() {
+        super(2021, 7);
+    }
+
+    @Override
+    public String resultPartOne() {
+        return "";
+    }
+
+    @Override
+    public String resultPartTwo() {
+        return "";
+    }
+}
