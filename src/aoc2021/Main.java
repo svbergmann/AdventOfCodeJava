a21fe64750237dacce2813e0dcfb4ec9c4c62730
Sven Bergmann <sven.bergmann@rwth-aachen.de>
2021-12-07 07:28:28 +0100

    Finished Day7

--- src/aoc2021/Main.java
+++ src/aoc2021/Main.java
@@ -1,19 +1,19 @@
 package aoc2021;
 
-import aoc2021.day6.Day6;
+import aoc2021.day7.Day7;
 import org.jetbrains.annotations.NotNull;
 import utils.Day;
 import utils.Utilities;
 
 public class Main {
 
-    public static void main(String[] args) {
-        printResults(new Day6());
-    }
+	public static void main(String[] args) {
+		printResults(new Day7());
+	}
 
-    public static void printResults(@NotNull Day day) {
-        System.out.println("Result of day " + day.number + ":");
-        System.out.println(Utilities.PART_ONE + day.resultPartOne());
-        System.out.println(Utilities.PART_TWO + day.resultPartTwo());
-    }
+	public static void printResults(@NotNull Day day) {
+		System.out.println("Result of day " + day.number + ":");
+		System.out.println(Utilities.PART_ONE + day.resultPartOne());
+		System.out.println(Utilities.PART_TWO + day.resultPartTwo());
+	}
 }
--- src/aoc2021/day7/Day7.java
+++ src/aoc2021/day7/Day7.java
@@ -2,19 +2,40 @@ package aoc2021.day7;
 
 import utils.Day;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Day7 extends Day {
 
-    public Day7() {
-        super(2021, 7);
-    }
+	private final ArrayList<Integer> crabPositions;
 
-    @Override
-    public String resultPartOne() {
-        return "";
-    }
+	public Day7() {
+		super(2021, 7);
+		this.crabPositions = new ArrayList<>(Arrays.stream(this.input.get(0).split(",")).map(Integer::parseInt).sorted().toList());
+	}
 
-    @Override
-    public String resultPartTwo() {
-        return "";
-    }
+	@Override
+	public String resultPartOne() {
+		return this.crabPositions.stream()
+				.map(i -> this.crabPositions
+						.stream()
+						.mapToInt(crabPosition -> Math.abs(crabPosition - i))
+						.sum()).min(Integer::compareTo)
+				.get()
+				+ "";
+	}
+
+	@Override
+	public String resultPartTwo() {
+		var fuelList = new ArrayList<Integer>();
+		for (int j = 0; j < this.crabPositions.get(this.crabPositions.size() - 1); j++) {
+			var fuel = 0;
+			for (var crabPosition : this.crabPositions) {
+				fuel += IntStream.rangeClosed(0, Math.abs(crabPosition - j)).sum();
+			}
+			fuelList.add(fuel);
+		}
+		return fuelList.stream().min(Integer::compareTo).get() + "";
+	}
 }
