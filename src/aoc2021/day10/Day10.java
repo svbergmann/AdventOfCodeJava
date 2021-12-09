29388c729c8850bf43eec8db724cf269809d2e1c
Sven Bergmann <sven.bergmann@rwth-aachen.de>
2021-12-09 11:43:00 +0100

    Prepared for Day10

---
+++ src/aoc2021/day10/Day10.java
@@ -0,0 +1,19 @@
+package aoc2021.day10;
+
+import utils.Day;
+
+public class Day10 extends Day {
+	public Day10() {
+		super(2021, 10);
+	}
+
+	@Override
+	public String resultPartOne() {
+		return null;
+	}
+
+	@Override
+	public String resultPartTwo() {
+		return null;
+	}
+}
