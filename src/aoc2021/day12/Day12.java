1a12da62aee53814452a037f871c9650d4483861
Sven Bergmann <sven.bergmann@rwth-aachen.de>
2021-12-13 13:55:44 +0100

    Worked on Day12

--- src/aoc2021/day12/Day12.java
+++ src/aoc2021/day12/Day12.java
@@ -3,134 +3,113 @@ package aoc2021.day12;
 import org.jetbrains.annotations.NotNull;
 import utils.Day;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
 
 
 public class Day12 extends Day {
 
-    private final HashSet<Cave> caves;
+	private final HashSet<Cave> caves;
+	private HashSet<ArrayList<Cave>> paths;
 
-    public Day12() {
-        super(2021, 12);
-        this.caves = new HashSet<>();
-        for (var s : this.example) {
-            var s1 = s.split("-");
-            Cave cave1 = null;
-            Cave cave2 = null;
-            for (var cave : this.caves) {
-                if (cave.name.equals(s1[0])) {
-                    cave1 = cave;
-                    continue;
-                }
-                if (cave.name.equals(s1[1])) {
-                    cave2 = cave;
-                }
-            }
-            if (cave1 != null) {
-                if (cave2 == null) cave2 = new Cave(s1[1]);
-                this.caves.add(cave2);
-            } else if (cave2 != null) {
-                cave1 = new Cave(s1[0]);
-                this.caves.add(cave1);
-            } else {
-                cave1 = new Cave(s1[0]);
-                this.caves.add(cave1);
-                cave2 = new Cave(s1[1]);
-                this.caves.add(cave2);
-            }
-            cave1.addConnection(cave2);
-        }
-    }
+	public Day12() {
+		super(2021, 12);
+		this.caves = new HashSet<>();
+		for (var s : this.example) {
+			var s1 = s.split("-");
+			Cave cave1 = null;
+			Cave cave2 = null;
+			for (var cave : this.caves) {
+				if (cave.name.equals(s1[0])) {
+					cave1 = cave;
+					continue;
+				}
+				if (cave.name.equals(s1[1])) {
+					cave2 = cave;
+				}
+			}
+			if (cave1 != null) {
+				if (cave2 == null) cave2 = new Cave(s1[1]);
+				this.caves.add(cave2);
+			} else if (cave2 != null) {
+				cave1 = new Cave(s1[0]);
+				this.caves.add(cave1);
+			} else {
+				cave1 = new Cave(s1[0]);
+				this.caves.add(cave1);
+				cave2 = new Cave(s1[1]);
+				this.caves.add(cave2);
+			}
+			cave1.addConnection(cave2);
+		}
+	}
 
-    @Override
-    public String resultPartOne() {
-        Cave startCave = null;
-        for (var cave : this.caves) {
-            if (cave.name.equals("start")) startCave = cave;
-        }
-        var res = this.getAllPaths(startCave);
+	@Override
+	public String resultPartOne() {
+		Cave startCave = null;
+		for (var cave : this.caves) if (cave.name.equals("start")) startCave = cave;
+		this.paths = new HashSet<>();
+		this.findAllPathsRek(startCave, new ArrayList<>(List.of(startCave)), this.paths);
+		return this.paths.toString();
+	}
 
-        return res.toString();
-    }
+	private void findAllPathsRek(Cave cave, ArrayList<Cave> path, HashSet<ArrayList<Cave>> bucket) {
+		if (cave.name.equals("end")) {
+			bucket.add(path);
+			return;
+		}
+		for (var nextCave : cave.connections) {
+			if (path.contains(nextCave) && !nextCave.bigCave) continue;
+			var copy = new ArrayList<>(path);
+			copy.add(nextCave);
+			this.findAllPathsRek(nextCave, copy, bucket);
+		}
+	}
 
-    private HashSet<ArrayList<Cave>> getAllPaths(Cave root) {
-        var st = new Stack<Cave>();
-        var res = new HashSet<ArrayList<Cave>>();
-        st.push(root);
+	@Override
+	public String resultPartTwo() {
+		return null;
+	}
 
-        while (!st.isEmpty()) {
-            var top = st.peek();
-            if (top.connections.isEmpty() || top.visited) {
-                st.pop();
-            } else {
-                for (var connection : top.connections) {
-                    if (connection != null) {
-                        if (connection.name.equals("end")) {
-                            st.push(connection);
-                            var tmpList = new ArrayList<>(st);
-                            res.add(tmpList);
-                            st.pop();
-                            System.out.println(tmpList);
-                            break;
-                        } else if (!connection.visited) {
-                            st.push(connection);
-                            if (!connection.bigCave) connection.visited = true;
-                            break;
-                        } else {
-                            st.push(connection);
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-        return res;
-    }
+	private static class Cave {
+		private final HashSet<Cave> connections;
+		String name;
+		boolean visited;
+		boolean bigCave;
 
-    @Override
-    public String resultPartTwo() {
-        return null;
-    }
+		public Cave(@NotNull String name) {
+			this.name = name;
+			this.connections = new HashSet<>();
+			this.visited = false;
+			this.bigCave = Character.isUpperCase(name.toCharArray()[0]);
+		}
 
-    private static class Cave {
-        private final HashSet<Cave> connections;
-        String name;
-        boolean visited;
-        boolean bigCave;
+		public void addConnection(Cave cave) {
+			this.connections.add(cave);
+		}
 
-        public Cave(@NotNull String name) {
-            this.name = name;
-            this.connections = new HashSet<>();
-            this.visited = false;
-            this.bigCave = Character.isUpperCase(name.toCharArray()[0]);
-        }
+		public boolean canMove(Cave cave) {
+			return this.connections.contains(cave);
+		}
 
-        public void addConnection(Cave cave) {
-            this.connections.add(cave);
-        }
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || this.getClass() != o.getClass()) return false;
+			Cave cave = (Cave) o;
+			return Objects.equals(this.name, cave.name);
+		}
 
-        public boolean canMove(Cave cave) {
-            return this.connections.contains(cave);
-        }
+		@Override
+		public int hashCode() {
+			return Objects.hash(this.name);
+		}
 
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || this.getClass() != o.getClass()) return false;
-            Cave cave = (Cave) o;
-            return Objects.equals(this.name, cave.name);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(this.name);
-        }
-
-        @Override
-        public String toString() {
-            return "Cave{" +
-                    "name='" + name + '\'' +
-                    '}';
-        }
-    }
+		@Override
+		public String toString() {
+			return this.name;
+		}
+	}
 }
