7ebe13fed7c066eb7ea76e941a826639c23f5789
Sven <sven.bergmann@rwth-aachen.de>
2021-12-19 19:50:00 +0100

    Prepared for 19

---
+++ src/aoc2021/day19/Day19.java
@@ -0,0 +1,19 @@
+package aoc2021.day19;
+
+import utils.Day;
+
+public class Day19 extends Day {
+	public Day19() {
+		super(2021, 19);
+	}
+
+	@Override
+	public String resultPartOne() {
+		return null;
+	}
+
+	@Override
+	public String resultPartTwo() {
+		return null;
+	}
+}
