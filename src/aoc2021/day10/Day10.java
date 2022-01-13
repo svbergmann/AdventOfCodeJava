dd7ec77584f1e078ffe520236973ebc28af856b3
Sven Bergmann <sven.bergmann@rwth-aachen.de>
2022-01-13 16:04:18 +0100

    Refactored some code and used newly learned switch statement

--- src/aoc2021/day10/Day10.java
+++ src/aoc2021/day10/Day10.java
@@ -10,137 +10,134 @@ import java.util.Stack;
 
 public class Day10 extends Day {
 
-    private static final HashMap<Character, Integer> CORRUPTED_CHARACTER_SCORES = new HashMap<>() {{
-        this.put(')', 3);
-        this.put(']', 57);
-        this.put('}', 1197);
-        this.put('>', 25137);
-    }};
+	private static final HashMap<Character, Integer> CORRUPTED_CHARACTER_SCORES = new HashMap<>() {{
+		this.put(')', 3);
+		this.put(']', 57);
+		this.put('}', 1197);
+		this.put('>', 25137);
+	}};
 
-    private static final HashMap<Character, Integer> INCOMPLETE_CHARACTER_SCORES = new HashMap<>() {{
-        this.put(')', 1);
-        this.put(']', 2);
-        this.put('}', 3);
-        this.put('>', 4);
-    }};
+	private static final HashMap<Character, Integer> INCOMPLETE_CHARACTER_SCORES = new HashMap<>() {{
+		this.put(')', 1);
+		this.put(']', 2);
+		this.put('}', 3);
+		this.put('>', 4);
+	}};
 
-    public Day10() {
-        super(2021, 10);
-    }
+	public Day10() {
+		super(2021, 10);
+	}
 
-    private static char getInvertedParenthesis(char parenthesis) {
-        var c = '0';
-        switch (parenthesis) {
-            case '(' -> c = ')';
-            case ')' -> c = '(';
-            case '[' -> c = ']';
-            case ']' -> c = '[';
-            case '<' -> c = '>';
-            case '>' -> c = '<';
-            case '{' -> c = '}';
-            case '}' -> c = '{';
-        }
-        return c;
-    }
+	private static char getInvertedParenthesis(char parenthesis) {
+		return switch (parenthesis) {
+			case '(' -> ')';
+			case ')' -> '(';
+			case '[' -> ']';
+			case ']' -> '[';
+			case '<' -> '>';
+			case '>' -> '<';
+			case '{' -> '}';
+			case '}' -> '{';
+			default -> '0';
+		};
+	}
 
-    private static @NotNull HashSet<String> getAllCorruptedLines(@NotNull HashSet<String> input) {
-        var res = new HashSet<String>();
-        for (var s : input) {
-            var stack = new Stack<Character>();
-            for (char c : s.toCharArray()) {
-                boolean illegalCharacterFound = false;
-                switch (c) {
-                    case '(', '[', '<', '{' -> stack.add(c);
-                    case ')', ']', '>', '}' -> {
-                        if (stack.isEmpty()) {
-                            illegalCharacterFound = true;
-                        } else if (c == getInvertedParenthesis(stack.peek())) {
-                            stack.pop();
-                        } else {
-                            illegalCharacterFound = true;
-                        }
-                    }
-                }
-                if (illegalCharacterFound) {
-                    res.add(s);
-                    break;
-                }
-            }
-        }
-        return res;
-    }
+	private static @NotNull HashSet<String> getAllCorruptedLines(@NotNull HashSet<String> input) {
+		var res = new HashSet<String>();
+		for (var s : input) {
+			var stack = new Stack<Character>();
+			for (char c : s.toCharArray()) {
+				boolean illegalCharacterFound = false;
+				switch (c) {
+					case '(', '[', '<', '{' -> stack.add(c);
+					case ')', ']', '>', '}' -> {
+						if (stack.isEmpty()) {
+							illegalCharacterFound = true;
+						} else if (c == getInvertedParenthesis(stack.peek())) {
+							stack.pop();
+						} else {
+							illegalCharacterFound = true;
+						}
+					}
+				}
+				if (illegalCharacterFound) {
+					res.add(s);
+					break;
+				}
+			}
+		}
+		return res;
+	}
 
-    private static long getCompletionScore(String s) {
-        var res = 0L;
-        for (var c : s.toCharArray()) {
-            res *= 5;
-            res += INCOMPLETE_CHARACTER_SCORES.get(c);
-        }
-        return res;
-    }
+	private static long getCompletionScore(String s) {
+		var res = 0L;
+		for (var c : s.toCharArray()) {
+			res *= 5;
+			res += INCOMPLETE_CHARACTER_SCORES.get(c);
+		}
+		return res;
+	}
 
-    @Override
-    public String resultPartOne() {
-        var illegalCharacters = new ArrayList<Character>();
-        for (var s : this.input) {
-            var stack = new Stack<Character>();
-            for (char c : s.toCharArray()) {
-                boolean illegalCharacterFound = false;
-                switch (c) {
-                    case '(', '[', '<', '{' -> stack.add(c);
-                    case ')', ']', '>', '}' -> {
-                        if (stack.isEmpty()) {
-                            illegalCharacters.add(c);
-                            illegalCharacterFound = true;
-                        } else if (c == getInvertedParenthesis(stack.peek())) {
-                            stack.pop();
-                        } else {
-//							System.out.println(s + " - Expected " + getInvertedParenthesis(stack.peek()) + " but found " + c + " instead");
-                            illegalCharacters.add(c);
-                            illegalCharacterFound = true;
-                        }
-                    }
-                }
-                if (illegalCharacterFound) break;
-            }
-        }
-        return illegalCharacters.stream()
-                .mapToInt(CORRUPTED_CHARACTER_SCORES::get)
-                .sum() + "";
-    }
+	@Override
+	public String resultPartOne() {
+		var illegalCharacters = new ArrayList<Character>();
+		for (var s : this.input) {
+			var stack = new Stack<Character>();
+			for (char c : s.toCharArray()) {
+				boolean illegalCharacterFound = false;
+				switch (c) {
+					case '(', '[', '<', '{' -> stack.add(c);
+					case ')', ']', '>', '}' -> {
+						if (stack.isEmpty()) {
+							illegalCharacters.add(c);
+							illegalCharacterFound = true;
+						} else if (c == getInvertedParenthesis(stack.peek())) {
+							stack.pop();
+						} else {
+							illegalCharacters.add(c);
+							illegalCharacterFound = true;
+						}
+					}
+				}
+				if (illegalCharacterFound) break;
+			}
+		}
+		return illegalCharacters.stream()
+		                        .mapToInt(CORRUPTED_CHARACTER_SCORES::get)
+		                        .sum() + "";
+	}
 
-    @Override
-    public String resultPartTwo() {
-        var inputSet = new HashSet<>(this.input);
-        var corruptedLinesSet = getAllCorruptedLines(inputSet);
-        inputSet.removeAll(corruptedLinesSet);
-        ArrayList<String> completionStringList = new ArrayList<>();
-        for (var s : inputSet) {
-            var stack = new Stack<Character>();
-            StringBuilder completionString = new StringBuilder();
-            for (char c : s.toCharArray()) {
-                switch (c) {
-                    case '(', '[', '<', '{' -> stack.add(c);
-                    case ')', ']', '>', '}' -> {
-                        if (c == getInvertedParenthesis(stack.peek())) {
-                            stack.pop();
-                        }
-                    }
-                }
-            }
-            while (!stack.isEmpty()) {
-                completionString.append(getInvertedParenthesis(stack.pop()));
-            }
-            completionStringList.add(completionString.toString());
-        }
-//		completionStringList.forEach(s -> System.out.println(s + " - " + getCompletionScore(s) + " total points."));
-        return (long) completionStringList.stream()
-                .mapToLong(Day10::getCompletionScore)
-                .sorted()
-                .skip((completionStringList.size() - 1) / 2)
-                .limit(2 - completionStringList.size() % 2)
-                .average()
-                .orElse(Double.NaN)
-                + "";
-    }
+	@Override
+	public String resultPartTwo() {
+		var inputSet = new HashSet<>(this.input);
+		var corruptedLinesSet = getAllCorruptedLines(inputSet);
+		inputSet.removeAll(corruptedLinesSet);
+		ArrayList<String> completionStringList = new ArrayList<>();
+		for (var s : inputSet) {
+			var stack = new Stack<Character>();
+			StringBuilder completionString = new StringBuilder();
+			for (char c : s.toCharArray()) {
+				switch (c) {
+					case '(', '[', '<', '{' -> stack.add(c);
+					case ')', ']', '>', '}' -> {
+						if (c == getInvertedParenthesis(stack.peek())) {
+							stack.pop();
+						}
+					}
+				}
+			}
+			while (!stack.isEmpty()) {
+				completionString.append(getInvertedParenthesis(stack.pop()));
+			}
+			completionStringList.add(completionString.toString());
+		}
+		return (long) completionStringList.stream()
+		                                  .mapToLong(Day10::getCompletionScore)
+		                                  .sorted()
+		                                  .skip((completionStringList.size() - 1) / 2)
+		                                  .limit(2 - completionStringList.size() % 2)
+		                                  .average()
+		                                  .orElse(Double.NaN)
+				+ "";
+	}
 }
--- src/aoc2021/day6/Day6.java
+++ src/aoc2021/day6/Day6.java
@@ -8,91 +8,89 @@ import java.util.stream.Collectors;
 
 public class Day6 extends Day {
 
-    public Day6() {
-        super(2021, 6);
-    }
+	public Day6() {
+		super(2021, 6);
+	}
 
-    @Override
-    public String resultPartOne() {
-        var list = new ArrayList<>(Arrays.stream(this.input.get(0)
-                        .split(","))
-                .map(s -> new Lanternfish(Integer.parseInt(s)))
-                .toList());
-        for (var i = 0; i < 80; i++) {
-            list.addAll(list.stream()
-                    .filter(Lanternfish::dayPassed)
-                    .map(l -> new Lanternfish(8))
-                    .collect(Collectors.toCollection(ArrayList::new)));
-        }
-        return list.size() + "";
-    }
+	@Override
+	public String resultPartOne() {
+		var list = new ArrayList<>(Arrays.stream(this.input.get(0).split(","))
+		                                 .map(s -> new Lanternfish(Integer.parseInt(s)))
+		                                 .toList());
+		for (var i = 0; i < 80; i++) {
+			list.addAll(list.stream()
+			                .filter(Lanternfish::dayPassed)
+			                .map(l -> new Lanternfish(8))
+			                .collect(Collectors.toCollection(ArrayList::new)));
+		}
+		return list.size() + "";
+	}
 
-    @Override
-    public String resultPartTwo() {
-        var amount0 = 0L;
-        var amount1 = 0L;
-        var amount2 = 0L;
-        var amount3 = 0L;
-        var amount4 = 0L;
-        var amount5 = 0L;
-        var amount6 = 0L;
-        var amount7 = 0L;
-        var amount8 = 0L;
+	@Override
+	public String resultPartTwo() {
+		var amount0 = 0L;
+		var amount1 = 0L;
+		var amount2 = 0L;
+		var amount3 = 0L;
+		var amount4 = 0L;
+		var amount5 = 0L;
+		var amount6 = 0L;
+		var amount7 = 0L;
+		var amount8 = 0L;
 
-        for (var s : this.input.get(0)
-                .toCharArray()) {
-            if (s != ' ') {
-                switch (s) {
-                    case '1' -> amount1++;
-                    case '2' -> amount2++;
-                    case '3' -> amount3++;
-                    case '4' -> amount4++;
-                    case '5' -> amount5++;
-                    case '6' -> amount6++;
-                }
-            }
-        }
+		for (var s : this.input.get(0).toCharArray()) {
+			if (s != ' ') {
+				switch (s) {
+					case '1' -> amount1++;
+					case '2' -> amount2++;
+					case '3' -> amount3++;
+					case '4' -> amount4++;
+					case '5' -> amount5++;
+					case '6' -> amount6++;
+				}
+			}
+		}
 
-        for (var i = 0; i < 256; i++) {
-            var newFishes = amount0;
-            amount0 = amount1;
-            amount1 = amount2;
-            amount2 = amount3;
-            amount3 = amount4;
-            amount4 = amount5;
-            amount5 = amount6;
-            amount6 = amount7 + newFishes;
-            amount7 = amount8;
-            amount8 = newFishes;
-        }
+		for (var i = 0; i < 256; i++) {
+			var newFishes = amount0;
+			amount0 = amount1;
+			amount1 = amount2;
+			amount2 = amount3;
+			amount3 = amount4;
+			amount4 = amount5;
+			amount5 = amount6;
+			amount6 = amount7 + newFishes;
+			amount7 = amount8;
+			amount8 = newFishes;
+		}
 
-        return amount0 + amount1 + amount2 + amount3 + amount4 + amount5 + amount6 + amount7 + amount8 + "";
-    }
+		return amount0 + amount1 + amount2 + amount3 + amount4 + amount5 + amount6 + amount7 + amount8 + "";
+	}
 
-    private static class Lanternfish {
-        private int internalTimer;
+	private static class Lanternfish {
+		private int internalTimer;
 
-        public Lanternfish(int internalTimer) {
-            this.internalTimer = internalTimer;
-        }
+		public Lanternfish(int internalTimer) {
+			this.internalTimer = internalTimer;
+		}
 
-        /**
-         * Simulates a passed Day.
-         *
-         * @return true if a new fish is created, else false
-         */
-        public boolean dayPassed() {
-            this.internalTimer--;
-            if (this.internalTimer == -1) {
-                this.internalTimer = 6;
-                return true;
-            }
-            return false;
-        }
+		/**
+		 * Simulates a passed Day.
+		 *
+		 * @return true if a new fish is created, else false
+		 */
+		public boolean dayPassed() {
+			this.internalTimer--;
+			if (this.internalTimer == -1) {
+				this.internalTimer = 6;
+				return true;
+			}
+			return false;
+		}
 
-        @Override
-        public String toString() {
-            return this.internalTimer + "";
-        }
-    }
+		@Override
+		public String toString() {
+			return this.internalTimer + "";
+		}
+	}
 }
